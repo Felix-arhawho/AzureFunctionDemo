22cb176fcfb5f15f2e85154ed30adc2d31c963da
Felix Arhawho <Felix.Arhawho@fcmb.com>
2022-01-21 07:53:36 +0100

    Project initiation

---
+++ AzureFunctionDemo.Function/SouthendSandbox.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AzureFunctionDemo.Function
+{
+    public static class SouthendSandbox
+    {
+        [FunctionName("SendBioData")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            return new OkObjectResult(requestBody);
+        }
+    }
+}
